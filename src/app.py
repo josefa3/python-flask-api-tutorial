1192d719c6695b7cdfc9aa322f113628412f51bd
josefa3 <sefaherrera391993@gmail.com>
2021-01-22 21:35:33 +0000

    se agregó un condicional en POST para recibir list o dict

--- src/app.py
+++ src/app.py
@@ -22,8 +22,13 @@ def hello_world():
 def add_new_todo():
     request_body = request.data #data es una de las propiedades de request, json
     decoded_object = json.loads(request_body) #convierte en objeto python
-    todos.append(decoded_object) #agrega los objetos python a todos
-    print("Incoming request with the following body", request_body)
+    if isinstance(decoded_object, list):
+        for task in decoded_object:
+            todos.append(task) #agrega los objetos python a todos
+    elif isinstance(decoded_object, dict):
+        todos.append(decoded_object)
+    else:
+        return "bad input", 400
     return jsonify(todos) #jsonifica otra vez
 
 @app.route('/todos/<int:position>', methods=['DELETE'])
@@ -33,13 +38,10 @@ def delete_todo(position):
     # return 'something'
     return jsonify(todos) #jsonifica la lista todos
 
-# @app.route('/todos', methods=['GET', 'POST']) #endpoint que se añadirá
-# def add_todo(): #esta funcion si debe ser distinto nombre, el endpoint puede ser igual
-#     return jsonify(todos)
-
 #siempre deben ser las últimas líneas, de lo contrario estarían fuera de lo que lanza el servidor
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
 
 #pipenv run python src/app.py para correr este archivo
+# el puerto 3245 se debe colocar publico para poder probar los métodos, sino da 401
 #bc run -l python3 -g incremental correr el servidor de los ejercicios
